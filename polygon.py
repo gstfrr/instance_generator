f50f4a283ce4f246f16e49f69b8cb9b7a376e108
Augusto Ferreira <gstfrr@gmail.com>
2019-05-26 22:08:06 -0300

    rotação implementada

--- polygon.py
+++ polygon.py
@@ -1,12 +1,33 @@
 import numpy as np
+import pywavefront
 
 
 class Polygon:
-    def __init__(self, seed, vertex, faces, color=None):
+    def __init__(self, seed, vertex, faces, color=None,volume=0):
         self.__seed = seed
         self.__vertex = vertex
         self.__faces = faces
         self.__color = color
+        self.__volume = volume
+
+    @staticmethod
+    def load_polygons_from_file(obj_file):
+        scene = pywavefront.Wavefront(obj_file, collect_faces=True)
+        scene.parse()
+
+        print(len(scene.vertices))
+
+        print('# of objects:\t', len(scene.mesh_list))  # list of objects
+        print('# of faces:\t', len(scene.mesh_list[0].faces))  # list of objects
+        print(scene.mesh_list[0].faces)
+        print(scene.vertices)
+
+        # p = Polygon(seed=seed,
+        #             vertex=vertex,
+        #             faces=faces,
+        #             color=color,
+        #             )
+        # return p
 
     @staticmethod
     def change_position(v_tuple, offset):
@@ -14,6 +35,30 @@ class Polygon:
         for i in range(len(v_tuple)):
             coordinate_list.append(v_tuple[i] + offset)
         return tuple(coordinate_list)
+
+    def rotate(self, alpha, beta):
+        # alpha, beta = np.pi, np.pi
+
+        rotation_z = np.array([[np.cos(alpha), -1 * np.sin(alpha), 0],
+                               [np.sin(alpha), np.cos(alpha), 0],
+                               [0, 0, 1]])
+
+        rotation_y = np.array([[np.cos(beta), 0, -1 * np.sin(beta)],
+                               [0, 1, 0],
+                               [np.sin(beta), 0, np.cos(beta)]])
+
+        rotation = np.matmul(rotation_z, rotation_y)
+
+        vertex = [*zip(*self.vertex)]
+
+        new_v = []
+        for i in vertex:
+            v = np.matmul(rotation, i)
+            new_v.append(tuple(v))
+
+        new_v = [*zip(*new_v)]
+
+        self.__vertex = new_v
 
     def change_vertex(self, offset):
         print(self.__vertex)
@@ -55,29 +100,35 @@ class Polygon:
     def color(self):
         return self.__color
 
-    def save(self, folder, index, max):
-        import sys
-        path = '/Users/augusto/Drive/UFLA/Mestrado/TPS/instance_generator/' + folder
-        filename = path + '/polygon-{0:04d}-{1:04d}.obj'.format(index, max)
-        with open(filename, 'w') as f:
-            sys.stdout = f
+    @property
+    def volume(self):
+        return self.__volume
 
-            v = [*zip(*self.vertex)]
-            print('# vertex')
-            for i in v:
-                print('v', i[0], i[1], i[2], sep='\t')
+    def save(self, index, total):
 
-            print('# faces')
-            for i in self.faces:
-                print('f\t', end='', sep='\t')
-                # print('\n')
-                for j in i:
-                    print(j + 1, end='\t')
-                print('\n', end='')
+        v = [*zip(*self.vertex)]
+        print('o\tpol_{0:03d}-{1:03d}'.format(index, total))
+        # print('# vertex')
+        for i in v:
+            print('v', i[0], i[1], i[2], sep='\t')
 
-            print('# color\nc\t', end='')
-            for i in self.color:
-                print(i, end='\t', sep='\t')
+        # print('# faces')
+        for i in self.faces:
+            print('f\t', end='', sep='\t')
+            # print('\n')
+            for j in i:
+                print(j + 1, end='\t')
+            print('\n', end='')
+
+        print('c\t', end='')
+        for i in self.color:
+            print(i, end='\t', sep='\t')
+
+        print('\nd\t', end='')
+        for i in self.seed:
+            print(i, end='\t', sep='\t')
+
+        print('\n\n')
 
     def show(self):
         print('seed:\t', self.seed)
