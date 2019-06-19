9cb27c2cc79ea92a1227d67b8baddf1eda08ea75
Augusto Ferreira <gstfrr@gmail.com>
2019-06-18 23:03:42 -0300

    script para executar os testes

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python3 model.py instances/problema0.data
+python3 model.py instances/problema1.data
+python3 model.py instances/problema2.data
+python3 model.py instances/problema3.data
+python3 model.py instances/problema4.data
+python3 model.py instances/problema5.data
+python3 model.py instances/problema6.data
+python3 model.py instances/problema7.data
+python3 model.py instances/problema8.data
+python3 model.py instances/problema9.data
+python3 model.py instances/problema10.data
+python3 model.py instances/problema11.data
+python3 model.py instances/problema12.data
+python3 model.py instances/problema13.data
+python3 model.py instances/problema14.data
+python3 model.py instances/problema15.data
+python3 model.py instances/problema16.data
