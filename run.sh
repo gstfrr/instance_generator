bb35ed714c7ef201412dc47fa859077b6268aa99
Augusto Ferreira <gstfrr@gmail.com>
2019-06-19 13:18:24 -0300

    script atualizado

--- run.sh
+++ run.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
+python3 3d-odrpp.py instances/problema0.data
+python3 3d-odrpp.py instances/problema1.data
+python3 3d-odrpp.py instances/problema2.data
+python3 3d-odrpp.py instances/problema3.data
+python3 3d-odrpp.py instances/problema4.data
+python3 3d-odrpp.py instances/problema5.data
+python3 3d-odrpp.py instances/problema6.data
+python3 3d-odrpp.py instances/problema7.data
+python3 3d-odrpp.py instances/problema8.data
+python3 3d-odrpp.py instances/problema9.data
+python3 3d-odrpp.py instances/problema10.data
+python3 3d-odrpp.py instances/problema11.data
+python3 3d-odrpp.py instances/problema12.data
+python3 3d-odrpp.py instances/problema13.data
+python3 3d-odrpp.py instances/problema14.data
+python3 3d-odrpp.py instances/problema15.data
+python3 3d-odrpp.py instances/problema16.data
+
+
 #python3 model.py instances/problema0.data
-python3 model.py instances/problema1.data
-python3 model.py instances/problema2.data
-python3 model.py instances/problema3.data
-python3 model.py instances/problema4.data
-python3 model.py instances/problema5.data
+#python3 model.py instances/problema1.data
+#python3 model.py instances/problema2.data
+#python3 model.py instances/problema3.data
+#python3 model.py instances/problema4.data
+#python3 model.py instances/problema5.data
 #python3 model.py instances/problema6.data
 #python3 model.py instances/problema7.data
 #python3 model.py instances/problema8.data
 #python3 model.py instances/problema9.data
 #python3 model.py instances/problema10.data
-python3 model.py instances/problema11.data
-python3 model.py instances/problema12.data
-python3 model.py instances/problema13.data
-python3 model.py instances/problema14.data
+#python3 model.py instances/problema11.data
+#python3 model.py instances/problema12.data
+#python3 model.py instances/problema13.data
+#python3 model.py instances/problema14.data
 #python3 model.py instances/problema15.data
 #python3 model.py instances/problema16.data
